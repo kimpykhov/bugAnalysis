015a1c2f39ae193254a3cbcd05b058d485546fd1
Kim Pykhov <pykhovk@gmail.com>
2026-01-05 22:43:48 +0200

    fixed generation for a time

--- src/data_generation/generate_dataset.py
+++ src/data_generation/generate_dataset.py
@@ -1,9 +1,10 @@
 import csv
 from pathlib import Path
 import random
+from datetime import datetime
 
 import distibutions
-from time_logic import fd, pd
+from time_logic import past_date, future_date
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 OUTPUT_PATH = PROJECT_ROOT / "data" / "raw" / "bugs_synthetic.csv"
@@ -14,8 +15,11 @@ with open(OUTPUT_PATH, "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["bug_id", "created_at", "closed_at", "priority", "severity", "component", "assignee_id", "labels",
                      "summary", "description"])
+    now = datetime.now()
     for i in range(1, 11, 1):
-        writer.writerow([i, 100, 100, random.choice(distibutions.priority),
+        created_at = past_date()
+        closed_at = future_date(created_at)
+        writer.writerow([i, created_at, closed_at, random.choice(distibutions.priority),
                          random.choice(distibutions.severity), random.choice(distibutions.component),
                          random.choice(distibutions.users), random.choice(distibutions.labels),
                          "fakesummary", "interim"])
